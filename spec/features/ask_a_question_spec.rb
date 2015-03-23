10983ffc285d3169fe67e8ce45c413c70eb8f7aa
Clem Freeman and Joey Sandine <clemfree@gmail.com>
2015-03-23 11:20:17 -0700

    Add feature test for ask question

---
+++ spec/features/ask_a_question_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe 'the ask a question process' do
+  it 'adds a new question' do
+    visit questions_path
+    click_on 'Ask Question'
+    fill_in 'Question', with: 'Why is the sky blue?'
+    click_on 'Ask'
+    expect(page).to have_content 'Questions'
+  end
+end
