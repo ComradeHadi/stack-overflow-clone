610c2bfc8242bc3a93aa9c946c80e3731a517415
Clem Freeman and Joey Sandine <clemfree@gmail.com>
2015-03-23 11:55:17 -0700

    Add new question functionality

--- spec/features/ask_a_question_spec.rb
+++ spec/features/ask_a_question_spec.rb
@@ -4,7 +4,7 @@ describe 'the ask a question process' do
   it 'adds a new question' do
     visit questions_path
     click_on 'Ask Question'
-    fill_in 'Question', with: 'Why is the sky blue?'
+    fill_in 'question_text', with: 'Why is the sky blue?'
     click_on 'Ask'
     expect(page).to have_content 'Questions'
   end
