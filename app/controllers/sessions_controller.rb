ee8f9ce68e72f129e2eb56e2d3eddf6ef7d0113a
Clem Freeman and Joey Sandine <jasandine@gmail.com>
2015-03-24 14:29:55 -0700

    Add log out of account functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,5 +15,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:notice] = "You've been logged out"
+    redirect_to root_path
   end
 end
