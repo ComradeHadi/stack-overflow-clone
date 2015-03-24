50ffbc6e10ccc04f0d1c5f85539bee9c8762021b
Clem Freeman and Joey Sandine <jasandine@gmail.com>
2015-03-24 11:31:38 -0700

    Add integration test for log-in

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,16 @@
 FactoryGirl.define do
+
   factory(:question) do
     text("How R U?")
   end
+
   factory(:answer) do
     text("Good")
   end
+
+  factory(:user) do
+    email('test@test.com')
+    password('password')
+  end
+  
 end
---
+++ spec/features/log_in_spec.rb
@@ -0,0 +1,12 @@
+require 'rails-helper'
+
+describe 'the log-in process' do
+  it 'logs a user in'
+    FactoryGirl.create(:user)
+    visit log_in_path
+    fill_in 'Email', with: 'test@test.com'
+    fill_in 'Password', with: 'password'
+    click_on 'Log in'
+    expect(page).to have_content 'You have been logged in'
+  end
+end
