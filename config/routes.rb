a677bb0553a6a64e8cc9da5231cf2de7c59b347b
Clem Freeman and Joey Sandine <jasandine@gmail.com>
2015-03-24 11:21:08 -0700

    Add sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root to: 'questions#index'
+  get '/log-in', to: 'sessions#new'
+  post '/log-in', to: 'sessions#create'
+  get '/log-out', to: 'sessions#destroy', as: :log_out
+
   resources :users
   resources :questions do
     resources :answers
