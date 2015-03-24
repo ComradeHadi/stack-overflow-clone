49b8baeb1ff5766c7a68fd555f360069c6128226
Clem Freeman and Joey Sandine <clemfree@gmail.com>
2015-03-24 11:09:27 -0700

    Confirm user login functionality, specs passing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,20 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Account created"
+      redirect_to root_path
+    else
+      flash[:alert] = "Try again!"
+      redirect_to :back
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
